bf7950809b97ab5d4acca3b53abfe30b41b19fbb
kundan kumar <kundan.kumar@oncehub.com>
2022-06-17 10:33:21 +0530

    added session

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :articles
   get '/register', to: 'users#new'
   resources :users, only: [:create]
+  get '/sign_in', to: 'sessions#new'
+  get '/sign_out', to: 'sessions#destroy'
+  resources :sessions, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
